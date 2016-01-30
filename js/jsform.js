da0a453ccba90842401360ea73b32571032c5230
SouAlien <gutoca77@gmail.com>
2016-01-30 15:04:37 -0200

    Formulario

---
+++ js/jsform.js
@@ -0,0 +1,88 @@
+ $("#cadastro").click(function() {
+     $(".formulario").css({
+         "position": "absolute"
+     });
+     $(".formulario").animate({
+         left: '100%'
+     }, "slow");
+     $(".formulario").css({
+         "position": "relative",
+         "display": "none"
+     });
+     $(".formulario2").animate({
+         right: '0px'
+     });
+
+     $(".formulario2").css({
+         "position": "relative",
+         "display": "block",
+     });
+
+     $(".bar").css({
+         "display": "block",
+     });
+     $(".bar2").animate({
+         width: '25%'
+     }, "slow");
+ });
+ $("#continuar").click(function() {
+     $(".formulario2").css({
+         "position": "absolute"
+     });
+     $(".formulario2").animate({
+         left: '100%'
+     }, "slow");
+
+     $(".formulario3").css({
+         "display": "block"
+     });
+     $(".formulario3").animate({
+         right: '0px'
+     });
+
+     $(".formulario3").css({
+         "position": "relative",
+         "display": "block",
+     });
+     $(".formulario2").css({
+         "position": "relative",
+         "display": "none"
+     });
+     $(".bar").css({
+         "display": "block",
+     });
+     $(".bar2").animate({
+         width: '50%'
+     });
+ });
+
+ $("#pagamento").click(function() {
+     $(".formulario3").css({
+         "position": "absolute"
+     });
+     $(".formulario3").animate({
+         left: '100%'
+     });
+
+     $(".formulario4").css({
+         "display": "block"
+     });
+     $(".formulario4").animate({
+         right: '0px'
+     });
+
+     $(".formulario4").css({
+         "position": "relative",
+         "display": "block",
+     });
+     $(".formulario3").css({
+         "position": "relative",
+         "display": "none"
+     });
+     $(".bar").css({
+         "display": "block",
+     });
+     $(".bar2").animate({
+         width: '75%'
+     }, "slow");
+ });
